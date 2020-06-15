56d502cbaecbf1bd24b0cfa6aac97267d419e178
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-15 23:39:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,6 +21,7 @@ def get_japanese_emoticon(file_path, english)
 end
 
 def get_english_meaning(file_path, japanese)
-
-
+  library = load_library(file_path)
+  emoticon = library.keys.find do |key|
+    library[key][:japanese]
 end
