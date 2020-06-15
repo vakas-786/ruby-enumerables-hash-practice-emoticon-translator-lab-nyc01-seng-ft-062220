ffc655501fd3a93d106643e5a31974ed8cd984af
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-15 23:36:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,7 @@ def get_japanese_emoticon(file_path, english)
   emoticion = library.keys.find do |key|
   library[key][:english] == emoticon
 end
-emoticon ? library[emoticon][:japanese] : "Sorry, that emoticon was not found"
+  emoticon ? library[emoticon][:japanese] : "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(file_path, japanese)
